6312e123718fb6e550c19e4b9bc540c8f4b558e7
Ariel Maniago <admaniago01@gmail.com>
2025-09-02 22:14:04 +0800

    first commit

---
+++ astro.config.ts
@@ -0,0 +1,38 @@
+// import node from "@astrojs/node";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, fontProviders } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "static",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  prefetch: {
+    prefetchAll: true
+  },
+
+  experimental:{
+    clientPrerender: true,
+    fonts:[
+      {
+        provider: fontProviders.fontshare(),
+        name: "Outfit",
+        cssVariable: "--font-outfit",
+        styles: ["normal"],
+         weights: ["100 900"],
+        fallbacks: ["Calibri"]
+      },
+      {
+        provider: fontProviders.google(),
+        name: "IBM Plex Sans",
+        cssVariable: "--font-ibm-plex-sans",
+        styles: ["normal", "italic"],
+        weights: ["100 700"],
+        fallbacks: ["system-ui", "sans-serif"]
+      },
+    ]
+  }
+});
