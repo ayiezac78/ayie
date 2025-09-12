6beeddf9aaa063b1f957418fe65fb2332e151700
Ariel Maniago <admaniago01@gmail.com>
2025-09-12 22:08:10 +0800

    chore: fixing typography

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,16 @@
 // import node from "@astrojs/node";
 
 import cloudflare from "@astrojs/cloudflare";
+import react from "@astrojs/react";
 import itsmatteomanfearlyHints from "@itsmatteomanf/astro-early-hints";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
   site: "https://ayie.github.io",
   base: "/",
   output: "server",
@@ -24,7 +28,7 @@ export default defineConfig({
         cssVariable: "--font-bebas-neue",
         styles: ["normal"],
         weights: ["100 900"],
-        fallbacks: ["Bahnschrift", "Arial", "sans-serif"],
+        fallbacks: ["Impact", "Arial Narrow", "sans-serif"],
       },
       {
         provider: fontProviders.google(),
@@ -32,15 +36,6 @@ export default defineConfig({
         cssVariable: "--font-roboto",
         styles: ["normal", "italic"],
         weights: ["100 700"],
-        fallbacks: [
-          "Inter",
-          "Helvetica Neue",
-          "Helvetica",
-          "Arial",
-          "Arial Nova",
-          "Nimbus Sans",
-          "sans-serif",
-        ],
       },
     ],
   },
@@ -49,9 +44,5 @@ export default defineConfig({
     imageService: "compile",
   }),
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
-
-  integrations: [itsmatteomanfearlyHints()],
+  integrations: [itsmatteomanfearlyHints(), react()],
 });
